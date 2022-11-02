f611733dbfddc039a2766ff7f509bd900b0d182d
Hemangi Chovatiya <hemangichovatiya17@gmail.com>
2022-11-02 20:34:26 +0000

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator
+{
+    static int a = 10;
+    static int b = 20;
+     public  static void addition()
+     {
+      System.out.println(a+b);
+     }
+
+    public static void main(String[] args)
+    {
+        addition();
+
+    }
+}
